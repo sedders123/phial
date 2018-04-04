f73a7b657a1135ac731895002b49c550ecac3236
James Seden Smith <sedders123@gmail.com>
2018-04-04 14:48:19 +0100

    Setup for PyPi

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup
 
 setup(
-    name='phial',
+    name='phial-slack',
     version='0.1.0',
     url='https://github.com/sedders123/phial/',
     license='MIT',
     author='James Seden Smith',
     author_email='sedders123@gmail.com',
-    description='A slack bot framework',
+    description='A Slack bot framework',
+    long_description=open('README.rst').read(),
     packages=['phial'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
+    python_requires='>=3.6',
+    keywords=['Slack', 'bot', 'Slackbot'],
     install_requires=[
         'slackclient==1.0.6',
         'Werkzeug==0.12.2',
     ],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
