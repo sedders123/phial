615859f5c17937e241750e2dabcc044d02a31d40
James Seden Smith <sedders123@gmail.com>
2018-04-05 18:07:46 +0100

    Added classifiers for all supported python versions

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
