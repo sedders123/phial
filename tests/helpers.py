8540b126d81aad74333d26227f3c68bbfc5aa90c
James Seden Smith <sedders123@gmail.com>
2019-02-19 21:53:44 +0000

    Add send_message tests

---
+++ tests/helpers.py
@@ -0,0 +1,26 @@
+from typing import Any
+import pytest # type: ignore # noqa
+
+
+def wildpatch(target: Any,
+              name: str,
+              value: Any = None,
+              raising: bool = True) -> None:
+    import inspect
+
+    if value is None:
+        if not isinstance(target, str):
+            raise TypeError("use setattr(target, name, value) or "
+                            "setattr(target, value) with target being a dotted"
+                            " import string")
+        value = name
+        name, target = derive_importpath(target, raising) # type: ignore # noqa
+
+    oldval = getattr(target, name, None)
+    if raising and oldval is None:
+        raise AttributeError("%r has no attribute %r" % (target, name))
+
+    # avoid class descriptors like staticmethod/classmethod
+    if inspect.isclass(target):
+        oldval = target.__dict__.get(name, None)
+    setattr(target, name, value)
