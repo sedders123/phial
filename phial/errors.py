cdbfb70b801c1a57d6018c6a84877334636b0d19
James Seden Smith <sedders123@gmail.com>
2019-09-04 23:26:01 +0100

    Add docstrings

--- phial/errors.py
+++ phial/errors.py
@@ -1,6 +1,13 @@
+"""phial's custom errors."""
+
+
 class ArgumentValidationError(Exception):
+    """Exception indicating argument validation has failed."""
+
     pass
 
 
 class ArgumentTypeValidationError(Exception):
+    """Exception indicating argument type validation has failed."""
+
     pass
